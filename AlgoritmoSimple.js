a31a8fdc347cf168007d2afebf6363f0cf08da57
Franco Albornoz <albfrank12@gmail.com>
2021-10-23 16:05:11 -0300

    Desafio Algoritmo Simple

---
+++ AlgoritmoSimple.js
@@ -0,0 +1,19 @@
+
+let nombreI= prompt("Ingrese su nombre")
+let apellidoI= prompt("Ingrese su apellido")
+console.log(nombreI)
+
+let saludo= alert("Hola su nombre completo es:  " + nombreI + " " +  apellidoI)
+
+alert("Ingrese 2 numeros para sumar y luego restar")
+let x1= prompt("Ingrese numero 1")
+let x2= prompt("Ingrese numero 2")
+
+let num1= parseInt(x1)
+let num2 = parseInt(x2)
+
+suma=num1+num2
+resta=num1-num2
+
+console.log(resta)
+console.log(suma)
